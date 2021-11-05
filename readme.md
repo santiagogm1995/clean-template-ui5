d7a82e722273f50858aa19435cc7f7080f67a8e5
santiagogm1995 <santiagogonzalezmota@gmail.com>
2021-11-05 09:08:21 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Clean template
+
+
+## Credits
+
+This project has been generated with 💙 and [easy-ui5](https://github.com/SAP)
---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(
+  ["com/clean/example/controller/BaseController"],
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.clean.example.controller.MainView", {});
+  }
+);
